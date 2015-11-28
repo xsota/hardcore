e68ab03027178da9fb2872d12e582a4d439f2eba
xsota <git@xsota.com>
2015-11-28 19:56:00 +0900

    コマンドを別classで処理するようにしてみた

--- src/main/java/com/xsota/Hardcore.java
+++ src/main/java/com/xsota/Hardcore.java
@@ -1,16 +1,17 @@
 package com.xsota;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.potion.PotionEffectType;
+
+
+import commands.Selfharm;
 
 public class Hardcore extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		getLogger().info("plugin has been enable.");
+		
+		//set command
+		getCommand("selfharm").setExecutor(new Selfharm());
 	}
 
 	@Override
@@ -18,25 +19,6 @@ public class Hardcore extends JavaPlugin {
 		getLogger().info("plugin has been disable.");
 	}
 
-	@Override
-	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-		Player player = null;
-		if (sender instanceof Player) {
-			player = (Player) sender;			
-		}
-		
-		if(player==null){
-			return false;
-		}
-				
-		if (command.getName().equalsIgnoreCase("selfharm")) {
-			sender.sendMessage(player.getName()+"はどこからか取り出した毒を飲んだ");
-			player.addPotionEffect(new PotionEffect(PotionEffectType.POISON,360000, 1));
-			return true;
-		}
 
-		return false;
-
-	}
 
 }
