cf6fd566bea3c611b01ffee416fd48cffdbe08a4
xsota <git@xsota.com>
2015-11-28 21:40:52 +0900

    プレイたーが死んだ時にメッセージだすようにしてみた。そのうちメッセージの代わりにBANするようにしよう

--- src/main/java/com/xsota/HardcoreListener.java
+++ src/main/java/com/xsota/HardcoreListener.java
@@ -1,9 +1,12 @@
 package com.xsota;
 
 
+import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.inventory.ItemStack;
@@ -18,4 +21,13 @@ public class HardcoreListener implements Listener{
 		player.sendMessage(LOGIN_MESSAGE);		
 	}
 	
+	/**
+	 * プレイヤーの死亡時に実行される
+	 * @param event
+	 */
+	@EventHandler(priority=EventPriority.HIGHEST)
+    public void onPlayerDeath(PlayerDeathEvent event) {
+		Player player = event.getEntity();
+		player.sendMessage("死んでしまうとはなさけない");	
+	}
 }
