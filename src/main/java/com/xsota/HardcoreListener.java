b6f354003f97c70fe5afa5693347d217c972c2be
xsota <git@xsota.com>
2016-01-04 22:14:52 +0900

    not make grave of op

--- src/main/java/com/xsota/HardcoreListener.java
+++ src/main/java/com/xsota/HardcoreListener.java
@@ -23,6 +23,7 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -39,6 +40,7 @@ public class HardcoreListener implements Listener {
 	JavaPlugin plugin;
 	ConsoleCommandSender console;
 	String dateFormat;
+
 	public HardcoreListener(JavaPlugin plugin) {
 		this.dateFormat = "yyyy/MM/dd HH:mm:ss";
 		this.console = Bukkit.getServer().getConsoleSender();
@@ -67,81 +69,85 @@ public class HardcoreListener implements Listener {
 	 */
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onPlayerDeath(PlayerDeathEvent event) {
-		String DEATH_MESSAGE = event.getDeathMessage();
-
 		// プレイヤー取得
 		final Player player = event.getEntity();
 
-		// banリスト取得
-		// BanList banList = Bukkit.getBanList(BanList.Type.NAME);
-
-		// BANする時間
-		//Calendar calendar = Calendar.getInstance();
-		//calendar.add(Calendar.HOUR, BAN_TIME);
-		
-		//Date expire = calendar.getTime();		
-		String expire = new SimpleDateFormat(this.dateFormat).format(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * BAN_TIME));		
-		
-		// 死亡時の座標にお墓
-		Location location = player.getLocation();
-		location.setY(location.getY());
-		Block block = location.getBlock();
-
-		block.setType(Material.SIGN_POST);
-		Sign sign = (Sign) block.getState();
-
-		if (DEATH_MESSAGE.length() >= 21) {
-			sign.setLine(1, DEATH_MESSAGE.substring(0, 21));
-			sign.setLine(2, DEATH_MESSAGE.substring(21));
-		} else {
-			sign.setLine(1, DEATH_MESSAGE);
-		}
-
-		sign.setLine(3, this.getNow());
-		sign.update();
-
-		// 次にログインした時に死亡画面だとなんかアレなので強制リスポーン
-		// player.spigot().respawn();
-
 		if (player.isOp() == false) {
+			String DEATH_MESSAGE = event.getDeathMessage();
+
+			// Inventory inventory = player.getInventory();
+			// banリスト取得
+			// BanList banList = Bukkit.getBanList(BanList.Type.NAME);
+
+			// BANする時間
+			// Calendar calendar = Calendar.getInstance();
+			// calendar.add(Calendar.HOUR, BAN_TIME);
+
+			// Date expire = calendar.getTime();
+			String expire = new SimpleDateFormat(this.dateFormat)
+					.format(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * BAN_TIME));
+
+			// 死亡時の座標にお墓
+			Location location = player.getLocation();
+			location.setY(location.getY());
+			Block block = location.getBlock();
+
+			block.setType(Material.SIGN_POST);
+			Sign sign = (Sign) block.getState();
+
+			if (DEATH_MESSAGE.length() >= 21) {
+				sign.setLine(1, DEATH_MESSAGE.substring(0, 21));
+				sign.setLine(2, DEATH_MESSAGE.substring(21));
+			} else {
+				sign.setLine(1, DEATH_MESSAGE);
+			}
+
+			sign.setLine(3, this.getNow());
+			sign.update();
+
+			// 次にログインした時に死亡画面だとなんかアレなので強制リスポーン
+			// player.spigot().respawn();
+
 			// プレイヤーBANリストに追加
-			// banList.addBan(player.getName(), DEATH_MESSAGE, expire, "Hardcore");
-			this.plugin.getConfig().set("BAN_PLAYERS."+player.getName(), expire);			
-			this.plugin.saveConfig();						
-	        
+			// banList.addBan(player.getName(), DEATH_MESSAGE, expire,
+			// "Hardcore");
+			this.plugin.getConfig().set("BAN_PLAYERS." + player.getName(), expire);
+			this.plugin.saveConfig();
+
 			// BANリストに追加するだけだとそのまま遊べちゃうのでkick
 			player.kickPlayer(DEATH_MESSAGE);
 		}
 	}
-	
+
 	/**
 	 * ban中のプレイヤーはJoinさせない
+	 * 
 	 * @param event
 	 */
 	@EventHandler
-	private void onLogin(PlayerLoginEvent event){
-		String ban = this.plugin.getConfig().getString("BAN_PLAYERS."+event.getPlayer().getName());
-		
-		if(ban == null){
+	private void onLogin(PlayerLoginEvent event) {
+		String ban = this.plugin.getConfig().getString("BAN_PLAYERS." + event.getPlayer().getName());
+
+		if (ban == null) {
 			return;
 		}
-		
+
 		Date now = new Date(System.currentTimeMillis());
-	
-		SimpleDateFormat simpleDateFromat = new SimpleDateFormat(this.dateFormat);	        
-	    Date banDate = new Date(); 
-	    
-	    try {
+
+		SimpleDateFormat simpleDateFromat = new SimpleDateFormat(this.dateFormat);
+		Date banDate = new Date();
+
+		try {
 			banDate = simpleDateFromat.parse(ban);
 		} catch (ParseException e) {
-			this.console.sendMessage("日付変換失敗:"+e);
+			this.console.sendMessage("日付変換失敗:" + e);
 		}
-	    
-		if(banDate.compareTo(now) > 0){
-			event.disallow(event.getResult().KICK_OTHER, "まだ死んでから"+this.BAN_TIME+"時間たってないよ");			
+
+		if (banDate.compareTo(now) > 0) {
+			event.disallow(event.getResult().KICK_OTHER, "まだ死んでから" + this.BAN_TIME + "時間たってないよ");
 		}
 	}
-	
+
 	/**
 	 * モンスター
 	 * 
@@ -159,7 +165,7 @@ public class HardcoreListener implements Listener {
 			zombie.setHealth(60);
 			return;
 		}
-		
+
 		if (entityType == EntityType.SKELETON) {
 			Skeleton skeleton = (Skeleton) entity;
 			skeleton.setCanPickupItems(true);
@@ -176,12 +182,14 @@ public class HardcoreListener implements Listener {
 	 * 
 	 * @param event
 	 */
-	/*@EventHandler
-	public void onLightningStrike(LightningStrikeEvent event) {
-		Location location = event.getLightning().getLocation();
-		event.getWorld().createExplosion(location.getX(), location.getY(), location.getZ(), (float) 2, true, true);
-
-	}*/
+	/*
+	 * @EventHandler public void onLightningStrike(LightningStrikeEvent event) {
+	 * Location location = event.getLightning().getLocation();
+	 * event.getWorld().createExplosion(location.getX(), location.getY(),
+	 * location.getZ(), (float) 2, true, true);
+	 * 
+	 * }
+	 */
 
 	/**
 	 * 現在時刻取得
